4d2e5b1f96a9c4745fae80a0a1904b1c343b71db
Terloaf <greenyryan2501@gmail.com>
2026-01-21 08:33:19 -0400

    Update Program.cs

--- GameProgramming_Challenge2Inheritance_GabeRyan/Program.cs
+++ GameProgramming_Challenge2Inheritance_GabeRyan/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,59 @@ namespace GameProgramming_Challenge2Inheritance_GabeRyan
     {
         static void Main(string[] args)
         {
+
+
+
+
         }
+
+
+
+        class Question
+        {
+            protected string _questionText;
+            protected string _answerText;
+
+
+            public Question (string questionText, string answerText)
+            {
+
+            }
+
+            public void Ask()
+            {
+                Console.WriteLine($"{_questionText}");
+            }
+            public bool CheckAnswer()
+            {
+                if(_questionText == _answerText)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+
+        class MultipleChoiceQuestion : Question
+        {
+
+        }
+
+
+
+        class TrueOrFalseQuestion : Question
+        {
+
+        }
+
+
+
+
+
     }
 }
